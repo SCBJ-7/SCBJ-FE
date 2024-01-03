6408be98e8e1a8886aa277b903a1ee9d45769441
seokmin <soekmin1998@naver.com>
2024-01-03 16:44:47 +0900

    Design: Pretendard Variable 폰트 추가 및 font-family 적용

--- src/main.tsx
+++ src/main.tsx
@@ -20,7 +20,7 @@ export const GlobalStyle = createGlobalStyle`
   
   * {
       box-sizing: border-box;
-      font-family: 'Pretendard Variable', sans-serif;
+      font-family: "Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
   }
     
   // hide scroll
