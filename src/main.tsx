a2aa04a48af98f127964e839d00db35487faa8a7
seokmin <soekmin1998@naver.com>
2024-01-03 17:20:25 +0900

    Design: 스크롤 관련 미사용 css 제거

--- src/main.tsx
+++ src/main.tsx
@@ -22,15 +22,6 @@ export const GlobalStyle = createGlobalStyle`
       box-sizing: border-box;
       font-family: "Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
   }
-    
-  // hide scroll
-  * {
-    -ms-overflow-style: none; /* IE and Edge */
-    scrollbar-width: none; /* Firefox */
-    ::-webkit-scrollbar {
-      display: none; /* Chrome, Safari, Opera*/
-    }
-  }
 `;
 
 const theme = {
