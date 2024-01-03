4653f36ddea63a653a717f28c8fe22629b5c2135
Bumang-Cyber <jove0729@naver.com>
2024-01-03 23:00:02 +0900

    feat: 최상위 컨테이너 구조 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
 import { Outlet } from "react-router-dom";
+import styled from "styled-components";
 
 function App() {
-  return <Outlet />;
+  return (
+    <SContainer>
+      {/* SHeader */}
+      <SWrapper>
+        <Outlet />;
+      </SWrapper>
+      {/* SFooter */}
+    </SContainer>
+  );
 }
 
 export default App;
+
+const SContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  background-color: ${(props) => props.theme.color.greyScale6};
+`;
+
+const SWrapper = styled.div`
+  max-width: 768px;
+  min-width: 360px;
+  width: 100%;
+  height: 100%;
+
+  background-color: white;
+`;
+
+// 예시입니다. 컴포넌트 리턴부의 {/* SHeader */}에 들어가야할 영역
+// const SHeader = styled.div`
+//   background-color: #464646
+//   width: 100%;
+//   height: 40px;
+// `;
