7b711e53916c3e16c4d56132f80936206a970254
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-01-05 03:21:15 +0900

    chore: 캐로셀 미리보기용

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { theme } from "./styles/theme.ts";
@@ -18,5 +17,5 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <RouterProvider router={router} />
       </ThemeProvider>
     </QueryClientProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
