e671bbed2a354a72d04b625452bc1ff66fa40f71
Bumang-Cyber <jove0729@naver.com>
2024-01-04 11:02:32 +0900

    fix: styled 파일 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,16 @@
 import { Outlet } from "react-router-dom";
-import styled from "styled-components";
+import * as S from "./App.styled";
 
 function App() {
   return (
-    <SContainer>
-      {/* <SHeader/> */}
-      <SWrapper>
+    <S.Container>
+      {/* <SHeader /> */}
+      <S.Wrapper>
         <Outlet />
-      </SWrapper>
+      </S.Wrapper>
       {/* <SFooter/> */}
-    </SContainer>
+    </S.Container>
   );
 }
 
 export default App;
-
-const SContainer = styled.div`
-  width: 100vw;
-  height: 100vh;
-
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-
-  background-color: ${(props) => props.theme.color.greyScale6};
-`;
-
-const SWrapper = styled.div`
-  max-width: 768px;
-  min-width: 360px;
-  width: 100%;
-  height: 100%;
-
-  background-color: white;
-`;
-
-/* 
-SHeader에 들어갈 컴포넌트 예시입니다.
-const SHeader = styled.div`
-  width: 100%;
-  height: 56px;
-  background-color: ${(props) => props.theme.color.greyScale2};
-`;
-*/
