e692245a7778241c6b5598bcd0e11ae056c10351
Bumang-Cyber <jove0729@naver.com>
2024-01-04 20:02:25 +0900

    fix: 불필요한 import제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { theme } from "./styles/theme.ts";
