312d8cfd96de5a4d7681c2ecab1ecdf7333d4af3
chaeminseok <minseok23491@naver.com>
2024-01-04 12:46:43 +0900

    feat: msw 세팅

--- src/pages/homePage/index.tsx
+++ src/pages/homePage/index.tsx
@@ -1,4 +1,41 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 const Home = () => {
+  //객실 정보
+  interface Room {
+    id: number;
+    name: string;
+    checkIn: string;
+    checkOut: string;
+    originalPrice: string;
+    salePrice: string;
+    roomPicture: string;
+  }
+
+  const [placeName, setPlaceName] = useState<string>("");
+  const [placeLoc, setPlaceLoc] = useState<string>("");
+  const [placePic, setPlacePic] = useState<string>("");
+  const [rooms, setRooms] = useState<Room[]>([]);
+  // const { id } = useParams<Params>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get("/api/roomId");
+        setRooms(res.data);
+
+        const res2 = await axios.get("/api/placeId");
+        setPlaceName(res2.data[0].name);
+        setPlaceLoc(res2.data[0].place);
+        setPlacePic(res2.data[0].picture);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  console.log("rooms", rooms);
   return (
     <>
       <div>home</div>
