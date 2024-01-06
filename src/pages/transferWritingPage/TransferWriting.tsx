194a1c5fb82944cc102f0259fd377bd209f549dc
Bumang-Cyber <jove0729@naver.com>
2024-01-06 20:42:56 +0900

    feat: api 연동

--- src/pages/transferWritingPage/TransferWriting.tsx
+++ src/pages/transferWritingPage/TransferWriting.tsx
@@ -1,7 +1,10 @@
+import { useEffect } from "react";
 import * as S from "./TransferWriting.styled";
 import TransferItem from "./transferItemList/transferItem/TransferItem";
 
 const TransferWriting = () => {
+  useEffect(() => {}, []);
+
   const list = [2, 2, 3, 4, 5];
 
   return (
