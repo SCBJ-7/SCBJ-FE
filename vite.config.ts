bd9c0269bb976a911a96f4fb0c02a37f6e737742
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-01-08 19:16:52 +0900

    chore: svgr 설치

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   test: {
     globals: true,
     environment: "jsdom",
