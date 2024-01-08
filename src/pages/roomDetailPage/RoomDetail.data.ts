a28e1a4418d59805c41302a229ead94ad54ee981
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-01-08 14:10:09 +0900

    design: room detail 페이지 레이아웃

---
+++ src/pages/roomDetailPage/RoomDetail.data.ts
@@ -0,0 +1,24 @@
+// FIXME: msw 사용 시 지우기
+export const dummyData = {
+  data: {
+    hotelName: "호텔 인 나인 강남",
+    roomName: "스탠다드 더블",
+    checkIn: "2023-12-24T15:00:00",
+    checkOut: "2023-12-25T10:00:00",
+    originalPrice: 400000,
+    sellingPrice: 320000,
+    standardPeople: 2,
+    maxPeople: 4,
+    bedType: "더블 베드",
+    roomTheme: {
+      parkingZone: true,
+      breakfast: true,
+      pool: false,
+      oceanView: false,
+    },
+    hotelAddress: "서울특별시 강남구 테헤란로 99길 9",
+    hotelInfoUrl: "https://place-site.yanolja.com/places/3001615",
+    saleStatus: false,
+  },
+  message: "상품 조회에 성공했습니다.",
+};
