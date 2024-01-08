af3704bd7287d6f4e4df251b6afaef1a06385356
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-01-08 22:10:47 +0900

    feat: msw 핸들러 설정

--- src/mocks/handler.ts
+++ src/mocks/handler.ts
@@ -1,8 +1,12 @@
 import { http, HttpResponse } from "msw";
 import dummyRoom from "./data/dummyRoom.json";
+import dummyRoomDetail from "./data/dummyRoomDetail.json";
 
 export const handlers = [
   http.get("/api/roomId", () => {
     return HttpResponse.json(dummyRoom);
+  }),
+  http.get("/v1/products/1", () => {
+    return HttpResponse.json(dummyRoomDetail);
   }),
 ];
