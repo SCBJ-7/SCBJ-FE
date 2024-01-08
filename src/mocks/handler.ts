cbfe6c6b92a5ca6f34c24ac4729fc7b5c8236806
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-01-08 22:10:47 +0900

    feat: msw 핸들러 설정

--- src/mocks/handler.ts
+++ src/mocks/handler.ts
@@ -1,5 +1,6 @@
 import { http, HttpResponse } from "msw";
 import dummyRoom from "./data/dummyRoom.json";
+import dummyRoomDetail from "./data/dummyRoomDetail.json";
 import reservationList from "./data/reservationList.json";
 
 export const handlers = [
@@ -10,11 +11,14 @@ export const handlers = [
     return HttpResponse.json(reservationList);
   }),
 
-  /* 
+  /*
     warning 없애기용.
     cdn에 폰트 요청하는걸 가로채지 못했다고 warning 띄우길래 적용했습니다.
   */
   http.get("*", (req) => {
     console.log(req);
+  }),
+  http.get("/v1/products/1", () => {
+    return HttpResponse.json(dummyRoomDetail);
   }),
 ];
