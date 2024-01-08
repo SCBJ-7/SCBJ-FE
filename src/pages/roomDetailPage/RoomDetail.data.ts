fd5805c839501af8b8c386ebabffd9bb008308a4
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-01-08 19:17:19 +0900

    design: 상품 상세 페이지 레이아웃

--- src/pages/roomDetailPage/RoomDetail.data.ts
+++ src/pages/roomDetailPage/RoomDetail.data.ts
@@ -2,6 +2,10 @@
 export const dummyData = {
   data: {
     hotelName: "호텔 인 나인 강남",
+    hotelImageUrl: [
+      "https://yaimg.yanolja.com/v5/2023/05/17/12/1280/6464c1e2d8acc7.52423534.jpg",
+      "https://yaimg.yanolja.com/v5/2022/06/08/18/1280/62a0ead93af4c0.61900529.jpg",
+    ],
     roomName: "스탠다드 더블",
     checkIn: "2023-12-24T15:00:00",
     checkOut: "2023-12-25T10:00:00",
--- src/pages/roomDetailPage/RoomDetail.tsx
+++ src/pages/roomDetailPage/RoomDetail.tsx
@@ -1,16 +1,25 @@
-import RoomInfo from "@pages/roomDetailPage/components/roomInfo/RoomInfo.tsx";
-import RoomNavBar from "@pages/roomDetailPage/components/roomNavBar/RoomNavBar.tsx";
+import Carousel from "@components/carousel/Carousel";
+import RoomInfo from "@pages/roomDetailPage/components/roomInfo/RoomInfo";
+import RoomNavBar from "@pages/roomDetailPage/components/roomNavBar/RoomNavBar";
 import { dummyData } from "./RoomDetail.data";
+
+import * as S from "./RoomDetail.style";
 
 const RoomDetail = () => {
   const { data } = dummyData;
 
   return (
-    <main>
-      {/* 캐로셀 위치 */}
+    <S.Container>
+      <Carousel
+        images={data.hotelImageUrl}
+        width="100%"
+        height={300}
+        arrows={true}
+        infinite={false}
+      />
       <RoomInfo room={data} />
       <RoomNavBar room={data} />
-    </main>
+    </S.Container>
   );
 };
 
