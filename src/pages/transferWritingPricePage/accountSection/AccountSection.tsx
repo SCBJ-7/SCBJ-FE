2693938d72d2a871e2c9d157d19f5d8f0f0fc973
Bumang-Cyber <jove0729@naver.com>
2024-01-11 03:51:35 +0900

    fix: 아직 안 쓰는 데이터 제거

--- src/pages/transferWritingPricePage/accountSection/AccountSection.tsx
+++ src/pages/transferWritingPricePage/accountSection/AccountSection.tsx
@@ -7,7 +7,8 @@ const AccountSection = () => {
       <S.Contents>
         <h1>입금 계좌</h1>
         <section>
-          <h2>{accountNumber ? accountNumber : "계좌를 등록해주세요"}</h2>
+          {/* <h2>{accountNumber ? accountNumber : "계좌를 등록해주세요"}</h2> */}
+          <h2>계좌를 등록해주세요</h2>
           <S.rightBtn></S.rightBtn>
         </section>
       </S.Contents>
