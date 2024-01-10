be46de87d5759d7115d4369e610c75abdd0bd0fd
seokmin <soekmin1998@naver.com>
2024-01-10 16:52:22 +0900

    Feat: 회원 정보 fetching api 연동

--- src/pages/myPage/manage/manageProfile/ManageProfile.tsx
+++ src/pages/myPage/manage/manageProfile/ManageProfile.tsx
@@ -2,37 +2,42 @@ import ManageEmail from "../manageEmail/ManageEmail";
 import ManageName from "../manageName/ManageName";
 import ManagePhoneNumber from "../managePhoneNumber/ManagePhoneNumber";
 import * as S from "./ManageProfile.style";
+import useProfileApi from "@/apis/getProfile";
+import type { ProfileData } from "./ManageProfile.type";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { AxiosError } from "axios";
 
 const ManageProfile = () => {
-  const { data } = dummy;
+  const { getProfileData } = useProfileApi();
+  const { isLoading, data, isError } = useSuspenseQuery<
+    ProfileData,
+    AxiosError
+  >({
+    queryKey: ["profile"],
+    queryFn: () => getProfileData("/v1/members"),
+  });
 
   return (
-    <S.ManageContainer>
-      <h1>나의 계정</h1>
-      <S.ManageInfoWrapper>
-        <ManageEmail prevEmail={data.email} />
-        <ManageName
-          prevName={data.name}
-          linkedToYanolja={data.linkedToYanolja}
-        />
-        <ManagePhoneNumber prevPhoneNumber={data.phone} />
-      </S.ManageInfoWrapper>
-    </S.ManageContainer>
+    <>
+      {isError && <div>error..</div>}
+      {isLoading && <div>Loading...</div>}
+      {data ? (
+        <S.ManageContainer>
+          <h1>나의 계정</h1>
+          <S.ManageInfoWrapper>
+            <ManageEmail prevEmail={data.email} />
+            <ManageName
+              prevName={data.name}
+              linkedToYanolja={data.linkedToYanolja}
+            />
+            <ManagePhoneNumber prevPhoneNumber={data.phone} />
+          </S.ManageInfoWrapper>
+        </S.ManageContainer>
+      ) : (
+        <div>Data Fetching Error</div>
+      )}
+    </>
   );
 };
 
 export default ManageProfile;
-
-const dummy = {
-  status: 200,
-  message: "성공적으로 회원 정보를 조회했습니다.",
-  data: {
-    id: 1,
-    email: "test@mail.com",
-    name: "윤석민",
-    phone: "10-1234-5678",
-    accountNumber: "123-456-7810",
-    bank: "농협",
-    linkedToYanolja: false,
-  },
-};
---
+++ src/pages/myPage/manage/manageProfile/ManageProfile.type.ts
@@ -0,0 +1,9 @@
+export interface ProfileData {
+  accountNumber: string | null;
+  bank: string | null;
+  email: string;
+  id: number;
+  linkedToYanolja: boolean;
+  phone: string;
+  name: string;
+}
