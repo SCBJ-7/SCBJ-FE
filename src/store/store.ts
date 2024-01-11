0ce3e8a6f35646af215ded9bd32ea2b5e2d64622
Bumang-Cyber <jove0729@naver.com>
2024-01-11 15:45:08 +0900

    feat: UserInfo 전역상태 추가, 계좌관리 컴포넌트 구현, 제출 가능여부 로직 구현, 데이터 타입 function 리턴타입 설정

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,12 +2,33 @@ import { IReservation } from "@/types/reservationList";
 import { IUserInfo } from "@/types/userInfo";
 import { create } from "zustand"; // create로 zustand를 불러옵니다.
 
-interface storeState {
-  selectedItem: IReservation;
-  setSelectedItem: (selectedItem: IReservation) => void;
+/* 
+  zustand 사용법:
+  기본적으로 storeState 타입을 만들고, 이걸 
+    create<StoreState>(() => ({ ...storeState }))
+  식으로 쓰면 됩니다!
+*/
+
+/*
+  storeState는 보통 아래처럼 구성합니다.
+  {
+    데이터,
+    데이터 조작 set함수1,
+    데이터 조작 set함수2,
+    데이터 조작 set함수3,
+    ...
+  }
+*/
+
+// 예약내역 선택 아이템 전역상태 타입
+interface ReservationState {
+  selectedItem: IReservation; // 데이터 기본값
+  setSelectedItem: (selectedItem: IReservation) => void; // 데이터 조작 set함수
 }
 
-export const useSelectedItemStore = create<storeState>((set) => ({
+// 예약내역 선택 아이템 전역상태 store
+export const useSelectedItemStore = create<ReservationState>((set) => ({
+  // 데이터 기본값
   selectedItem: {
     reservationId: 0,
     hotelName: "",
@@ -20,21 +41,25 @@ export const useSelectedItemStore = create<storeState>((set) => ({
     remainingTimes: 0,
     imageUrl: "",
   },
-  setSelectedItem: (selectedItem) =>
+  // 데이터 조작 set 함수
+  setSelectedItem: (updatedItem) =>
     set((state) => ({
       selectedItem: {
         ...state.selectedItem,
-        ...selectedItem,
+        ...updatedItem,
       },
     })),
 }));
 
+// 유저정보 전역상태
 interface UserState {
   userInfo: IUserInfo;
   setUserInfo: (userInfo: IUserInfo) => void;
 }
 
+// 유저정보 전역상태 store
 export const useUserInfoStore = create<UserState>((set) => ({
+  // 데이터 기본값
   userInfo: {
     id: "",
     email: "",
@@ -44,11 +69,12 @@ export const useUserInfoStore = create<UserState>((set) => ({
     bank: null,
     linkToYanolja: false,
   },
-  setUserInfo: (userInfo) =>
+  // 데이터 조작 set 함수
+  setUserInfo: (updatedInfo) =>
     set((state) => ({
       userInfo: {
         ...state.userInfo,
-        ...userInfo,
+        ...updatedInfo,
       },
     })),
 }));
