e26b736523d461b2240f672e4613ac6003313ae1
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-01-12 23:58:14 +0900

    feat: 제출 이벤트 처리

---
+++ src/pages/connectYanoljaAccountPage/components/submitButton/SubmitButton.tsx
@@ -0,0 +1,51 @@
+import { useFormContext } from "react-hook-form";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import * as S from "./SubmitButton.style";
+import { PATH } from "@constants/path";
+import { useNavigate } from "react-router-dom";
+
+const SubmitButton = () => {
+  const {
+    handleSubmit,
+    getValues,
+    formState: { isValid },
+  } = useFormContext();
+  const navigate = useNavigate();
+
+  const { mutate: submitMutation } = useMutation({
+    mutationFn: (email) =>
+      // FIXME: axios instance 정해지면 교체예정
+      axios.post("https://3.34.147.187.nip.io/v1/members/yanolja", { email }),
+    onSuccess: () => {
+      navigate(PATH.YANOLJA_ACCOUNT_VERIFY + "/success", {
+        state: { success: true },
+        replace: true,
+      });
+    },
+    onError: () => {
+      // 에러 핸들링 필요
+    },
+  });
+
+  const onSubmit = () => {
+    const email = getValues("email");
+    if (!email) return;
+
+    submitMutation(email);
+  };
+
+  return (
+    <S.BottomWrapper>
+      <S.Button
+        type="submit"
+        onClick={handleSubmit(onSubmit)}
+        data-disabled={isValid ? null : ""}
+      >
+        계정 연동하기
+      </S.Button>
+    </S.BottomWrapper>
+  );
+};
+
+export default SubmitButton;
