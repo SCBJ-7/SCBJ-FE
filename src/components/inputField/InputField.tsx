2af6e658b6b3641e9a045aa362fa09fb32bddc28
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-01-12 23:55:45 +0900

    feat: Input 컴포넌트 구현

---
+++ src/components/inputField/InputField.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import * as S from "./InputField.style";
+import {
+  FieldValues,
+  useController,
+  UseControllerProps,
+} from "react-hook-form";
+import type { ColorKeys } from "@/styles/theme";
+
+interface InputFieldProps<TFieldValues extends FieldValues>
+  extends UseControllerProps<TFieldValues> {
+  label: string;
+  type?: React.HTMLInputTypeAttribute;
+  placeholder?: string;
+  onButtonClick?: () => void;
+  buttonText?: string;
+  buttonVariant?: "outline" | "solid";
+  buttonColor?: ColorKeys;
+  successMessage?: string;
+  isSuccess?: boolean;
+}
+
+function InputField<TFieldValues extends FieldValues>({
+  label,
+  type = "text",
+  placeholder,
+  onButtonClick,
+  buttonText,
+  buttonVariant = "outline",
+  buttonColor = "percentOrange",
+  successMessage,
+  isSuccess = false,
+  ...useControllerProps
+}: InputFieldProps<TFieldValues>) {
+  const {
+    field,
+    fieldState: { error },
+  } = useController(useControllerProps);
+
+  return (
+    <S.InputWrapper>
+      <S.Label>{label}</S.Label>
+      <div className="input_wrapper">
+        <S.Input type={type} placeholder={placeholder} {...field} />
+        {buttonText && (
+          <S.ConFirmButton
+            type="button"
+            onClick={onButtonClick}
+            color={buttonColor}
+            variant={buttonVariant}
+          >
+            {buttonText}
+          </S.ConFirmButton>
+        )}
+      </div>
+      {error && <S.InputCaption error>{error.message}</S.InputCaption>}
+      {!error && isSuccess && successMessage && (
+        <S.InputCaption>{successMessage}</S.InputCaption>
+      )}
+    </S.InputWrapper>
+  );
+}
+
+export default InputField;
