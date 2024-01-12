d9174b9e5ed6a2012859fb89870c153e607f6721
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-01-12 23:57:26 +0900

    feat: 체크박스 트리 구현

---
+++ src/pages/connectYanoljaAccountPage/components/termsAgreementForm/TermsAgreementForm.style.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const SubWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: calc(100% - 40px);
+
+  margin-top: 5rem;
+`;
