b1f586819f83605314f3bbb2f783b2cfc8aa75e1
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-01-13 17:27:47 +0900

    chore: 경로 수정

---
+++ src/hooks/common/useIsVisible.ts
@@ -0,0 +1,42 @@
+import { RefCallback, useEffect, useState } from "react";
+
+type UseIsVisibleReturnType = [RefCallback<HTMLDivElement>, boolean];
+
+interface UseIsVisibleProps {
+  options: {
+    threshold: number;
+    rootMargin: string;
+  };
+  initialVisible: boolean;
+}
+
+const useIsVisible = (props: UseIsVisibleProps): UseIsVisibleReturnType => {
+  const { options, initialVisible } = props;
+  const [visibleRef, setVisibleRef] = useState<HTMLDivElement | null>(null);
+  const [isVisible, setIsVisible] = useState(initialVisible);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      setIsVisible(entry.isIntersecting);
+    }, options);
+
+    if (visibleRef) {
+      observer.observe(visibleRef);
+      return;
+    }
+
+    return () => {
+      if (visibleRef) {
+        observer.unobserve(visibleRef);
+      }
+    };
+  }, [options, visibleRef]);
+
+  const setRefCallback: RefCallback<HTMLDivElement> = (node) => {
+    setVisibleRef(node);
+  };
+
+  return [setRefCallback, isVisible];
+};
+
+export default useIsVisible;
