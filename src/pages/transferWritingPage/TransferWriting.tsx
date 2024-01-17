89c450baef564fb41c4c4e664c34be2ea3ef1c05
Bumang-Cyber <jove0729@naver.com>
2024-01-17 14:52:59 +0900

    feat: 인증 안 한 경우 페이지 작업

--- src/pages/transferWritingPage/TransferWriting.tsx
+++ src/pages/transferWritingPage/TransferWriting.tsx
@@ -2,20 +2,30 @@ import * as S from "./TransferWriting.style";
 import TransferItem from "./transferItem/TransferItem";
 
 import { fetchTransferItems } from "@/apis/fetchTransferItems";
-import { useToastStore } from "@/store/store";
+import { useToastStore, useUserInfoStore } from "@/store/store";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { AnimatePresence } from "framer-motion";
 import { useEffect } from "react";
+import { fetchUserInfo } from "@apis/fetchUserInfo.ts";
 
 const TransferWriting = () => {
-  const { data } = useSuspenseQuery({
-    queryKey: ["TransferItemList"],
+  const setToastConfig = useToastStore((state) => state.setToastConfig);
+  const userInfo = useUserInfoStore((state) => state.userInfo);
+
+  const { data: userData } = useSuspenseQuery({
+    queryKey: ["UserInfo"],
+    queryFn: fetchUserInfo,
+  });
+
+  const { data: transferData } = useSuspenseQuery({
+    queryKey: ["TransferItemList", userData?.id],
     queryFn: fetchTransferItems,
   });
 
-  const setToastConfig = useToastStore((state) => state.setToastConfig);
+  const token = localStorage.getItem("accessToken");
 
   useEffect(() => {
+    if (!userData?.linkToYanolja || !token) return;
     setToastConfig({
       isShow: true,
       isError: false,
@@ -28,13 +38,21 @@ const TransferWriting = () => {
         strings: [<>야놀자</>, "에서 예약하신 상품만 판매 가능해요."],
       });
     }, 3000);
-  }, [setToastConfig]);
+  }, [setToastConfig, token, userData?.linkToYanolja]);
+
+  if (!token) {
+    return <div>로그인이 필요합니다.</div>;
+  }
+
+  if (!userInfo.linkToYanolja) {
+    return <div>야놀자 연동이 필요합니다.</div>;
+  }
 
   return (
     <>
       <S.Subtitle>판매할 내역을 선택해주세요.</S.Subtitle>
       <S.TransferItemList>
-        {data?.map((item) => {
+        {transferData?.map((item) => {
           return (
             <AnimatePresence key={item.reservationId}>
               <TransferItem
