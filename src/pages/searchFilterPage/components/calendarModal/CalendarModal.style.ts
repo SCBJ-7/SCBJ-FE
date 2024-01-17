34f3537cd441cb8b30644abb2c0f95a9dbfe7d49
chaeminseok <minseok23491@naver.com>
2024-01-17 19:47:18 +0900

    feat: 달력 모달 커스텀

--- src/pages/searchFilterPage/components/calendarModal/CalendarModal.style.ts
+++ src/pages/searchFilterPage/components/calendarModal/CalendarModal.style.ts
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import CloseButton from "@assets/icons/ic_close-button.svg?react";
+import ResetIcon from "@assets/icons/ic_reset.svg?react";
+
 export const ModalContainer = styled.div`
   display: flex;
   justify-content: center;
@@ -22,6 +24,7 @@ export const ModalContent = styled.div`
   padding: 56px 20px 0 20px;
   display: flex;
   justify-content: center;
+
   ${({ theme }) => theme.scroll};
 `;
 
@@ -46,5 +49,142 @@ export const ModalTitle = styled.div`
 `;
 
 export const ModalCloseButton = styled(CloseButton)`
+  cursor: pointer;
+`;
+
+export const CalendarContainer = styled.div`
+  box-sizing: border-box;
+
+  width: 360px;
+  .react-datepicker {
+    padding: 0 20px;
+    border: none;
+  }
+  .react-datepicker__day--disabled {
+    color: black;
+    opacity: 0.3;
+  }
+  .sunday {
+    color: #ff4949;
+  }
+  .saturday {
+    color: #0a76da;
+  }
+
+  .react-datepicker__navigation {
+    top: 2.5rem;
+  }
+  .react-datepicker__navigation--previous {
+    position: absolute;
+    left: 300px;
+  }
+
+  .react-datepicker__month-container {
+    width: 100%;
+    height: auto;
+  }
+  .react-datepicker__header {
+    background-color: white;
+    border: none;
+  }
+  .react-datepicker__week {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 1rem;
+  }
+  .react-datepicker__month-container {
+    border-bottom: 1px solid ${({ theme }) => theme.color.greyScale6};
+  }
+  .react-datepicker__current-month {
+    ${({ theme }) => theme.typo.title2}
+    width: 320px;
+    display: flex;
+    justify-content: space-between;
+  }
+  .react-datepicker__month-container {
+    padding: 32px 0;
+  }
+  .react-datepicker__day {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 30px;
+    height: 30px;
+  }
+  .react-datepicker__day-names {
+    width: 320px;
+    display: flex;
+    justify-content: space-between;
+    margin-top: 1.5rem;
+    margin-bottom: 1.5rem;
+    border-bottom: 1px solid ${({ theme }) => theme.color.greyScale6};
+    font-size: 14px;
+  }
+  .react-datepicker__month {
+    border: none;
+  }
+  .react-datepicker__day--range-start {
+    border-radius: 50% !important;
+    color: white !important;
+    background-color: ${({ theme }) => theme.color.percentOrange} !important;
+  }
+  .react-datepicker__day--in-range {
+    background-color: white;
+    color: ${({ theme }) => theme.color.percentOrange};
+    border: 2px solid ${({ theme }) => theme.color.percentOrange};
+    font-weight: bold;
+  }
+  .react-datepicker__day--keyboard-selected {
+    border-radius: 50% !important;
+    color: white;
+    background-color: ${({ theme }) => theme.color.percentOrange};
+  }
+  .react-datepicker__day--in-selecting-range {
+    background-color: white;
+    color: ${({ theme }) => theme.color.percentOrange};
+    border: 2px solid ${({ theme }) => theme.color.percentOrange};
+    font-weight: bold;
+  }
+`;
+
+export const FilterBottom = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 768px;
+  min-width: 360px;
+  width: 100%;
+  position: absolute;
+  height: 80px;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  border-top: 1px solid ${({ theme }) => theme.color.greyScale6};
+  padding: 20px;
+  z-index: 1000;
+  background-color: white;
+`;
+
+export const ResetButtonContent = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 70px;
+  height: 24px;
+  color: ${({ theme }) => theme.color.greyScale3};
+  cursor: pointer;
+  > div:first-child {
+  }
+`;
+export const ResetButton = styled(ResetIcon)``;
+export const SearchButton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 242px;
+  height: 39px;
+  color: white;
+  background-color: ${({ theme }) => theme.color.percentOrange};
+  border-radius: 12px;
   cursor: pointer;
 `;
--- src/pages/searchFilterPage/components/calendarModal/CalendarModal.tsx
+++ src/pages/searchFilterPage/components/calendarModal/CalendarModal.tsx
@@ -1,6 +1,9 @@
 import * as S from "./CalendarModal.style";
 import { ko } from "date-fns/locale";
-import React, { useRef } from "react";
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+import { subDays, isSaturday, isSunday, format } from "date-fns";
+import "react-datepicker/dist/react-datepicker.css";
 
 interface CalendarModalProps {
   setDateIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
@@ -17,21 +20,84 @@ const CalendarModal = ({
   setCheckIn,
   setCheckOut,
 }: CalendarModalProps) => {
+  const [startDate, setStartDate] = useState<Date | null>(new Date());
+  const [endDate, setEndDate] = useState<Date | null>(null);
+
   const handleModalClose = () => {
     setDateIsModalOpen(false);
   };
 
+  const onChange = (dates: [Date | null, Date | null] | null) => {
+    if (dates) {
+      const [start, end] = dates;
+      console.log(start);
+      setStartDate(start);
+      setEndDate(end !== start ? end : null);
+    }
+  };
+
+  const onReset = () => {
+    // 초기화 버튼 클릭 시 날짜 초기화
+    setStartDate(null);
+    setEndDate(null);
+  };
+
+  const dayClassName = (date: Date) => {
+    // Apply custom classes based on the day of the week
+    if (isSunday(date)) {
+      return "sunday";
+    } else if (isSaturday(date)) {
+      return "saturday";
+    }
+    return undefined;
+  };
+
+  const selectedDatesText =
+    startDate && endDate
+      ? `${format(startDate, "yy.MM.dd (EEE)", { locale: ko })} ~ ${format(
+          endDate,
+          "yy.MM.dd (EEE)",
+          { locale: ko },
+        )}`
+      : "검색하기";
+
   return (
-    <S.ModalContainer>
-      <S.ModalTop>
-        <div></div>
-        <S.ModalTitle>일정</S.ModalTitle>
-        <div>
-          <S.ModalCloseButton onClick={handleModalClose} />
-        </div>
-      </S.ModalTop>
-      <S.ModalContent>\</S.ModalContent>
-    </S.ModalContainer>
+    <>
+      <S.ModalContainer>
+        <S.ModalTop>
+          <div></div>
+          <S.ModalTitle>일정</S.ModalTitle>
+          <div>
+            <S.ModalCloseButton onClick={handleModalClose} />
+          </div>
+        </S.ModalTop>
+        <S.ModalContent>
+          <S.CalendarContainer>
+            <DatePicker
+              locale={ko}
+              selectsRange
+              startDate={startDate}
+              endDate={endDate}
+              minDate={subDays(new Date(), 0)}
+              onChange={onChange}
+              isClearable={true}
+              inline
+              shouldCloseOnSelect={false}
+              dayClassName={dayClassName}
+            />
+          </S.CalendarContainer>
+        </S.ModalContent>
+      </S.ModalContainer>
+      <S.FilterBottom>
+        <S.ResetButtonContent onClick={onReset}>
+          <div>초기화</div>
+          <div>
+            <S.ResetButton />
+          </div>
+        </S.ResetButtonContent>
+        <S.SearchButton>{selectedDatesText}</S.SearchButton>
+      </S.FilterBottom>
+    </>
   );
 };
 
