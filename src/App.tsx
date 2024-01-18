325cd362d090d82b9bd56081604ff1077cafc4d1
seokmin <soekmin1998@naver.com>
2024-01-19 05:39:21 +0900

    Fix: App.tsx 내에서 location에 따른 setToastConfig 초기화 삭제
    불필요한 로직

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Layout from "./components/layout/Layout.tsx";
 import Toast from "./components/toast/Toast.tsx";
 
 import { useToastStore } from "./store/store.ts";
 import { AnimatePresence } from "framer-motion";
-import { useEffect } from "react";
 
 function App() {
   const toastConfig = useToastStore((state) => state.config);
-  const setToastConfig = useToastStore((state) => state.setToastConfig);
-  const location = useLocation();
-
-  useEffect(() => {
-    setToastConfig({
-      isShow: false,
-      isError: false,
-      strings: toastConfig.strings,
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [location.pathname]);
 
   return (
     <Layout>
