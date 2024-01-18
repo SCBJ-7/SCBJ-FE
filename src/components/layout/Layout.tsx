45fa8601a68d450c355dfd25a401116d8415538b
seokmin <soekmin1998@naver.com>
2024-01-19 05:42:33 +0900

    Fix: 계좌 관련 페이지 헤더 및 하단 Nav 수정

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -10,17 +10,30 @@ interface ChildrenProps {
 }
 
 const Layout = ({ children }: ChildrenProps) => {
-  const { pathname } = useLocation();
+  const { pathname, search } = useLocation();
 
   // FIXME: 헤더, 네비바 특정 페이지에서만 보이지 않도록 수정 필요
   // 이럼 account/yanolja/verify 이런 하위 페이지도 같이 include 됨
 
   const pathsToExclude = [PATH.LOGIN, PATH.DETAIL_ROOM, PATH.YANOLJA_ACCOUNT];
-  const isHeaderOn = !pathsToExclude.some((path) => pathname.includes(path));
+  let isHeaderOn = !pathsToExclude.some((path) => pathname.includes(path));
   let isBottomNavOn = !pathsToExclude.some((path) => pathname.includes(path));
 
   if (pathname.includes(PATH.WRITE_TRANSFER_PRICE as string)) {
     isBottomNavOn = false;
+  }
+
+  if (pathname.includes(PATH.MANAGE_ACCOUNT)) {
+    if (pathname === PATH.ACCOUNT_EDIT) {
+      isHeaderOn = false;
+    }
+    if (search === "?step=termsAgreement") {
+      isHeaderOn = false;
+      isBottomNavOn = false;
+    }
+    if (search === "?step=enterAccount") {
+      isHeaderOn = false;
+    }
   }
 
   return (
