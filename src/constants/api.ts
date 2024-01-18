0ee9fb18df2cf21c9052cd879c7ba83f6c47c9ff
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-01-19 03:00:47 +0900

    fix: instance 사용x, return 값 수정

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,3 +1,5 @@
+export const BASE_URL = "https://3.34.147.187.nip.io";
+
 export const END_POINTS = {
   ROOM: (roomId: string) => `/v1/products/${roomId}`,
   RESERVATION: "/v1/reservations",
@@ -6,11 +8,12 @@ export const END_POINTS = {
   PURCHASE_DETAIL: (purchaseId: string) => `/v1/purchase-detail/${purchaseId}`,
   EMAIL: "/v1/members/email",
   YANOLJA: "/v1/members/yanolja",
+  PAYMENT: (productId: string) => `/v1/products/${productId}/payments`,
   NEW_TOKEN: "/v1/token/refresh",
 } as const;
 
 export const ERROR_CODE = {
-  INVALID_TOKEN: 400,
+  INVALID_TOKEN: 402,
   EXPIRED_TOKEN: 401,
 } as const;
 
