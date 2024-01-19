3028f62621e09cce91f5d8095ae6011680fb40c2
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-01-19 12:34:20 +0900

    design: 결제하기 버튼 마크업

---
+++ src/pages/paymentPage/components/paymentButton/PaymentButton.style.ts
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+import { hexToRgba } from "@/utils/hexTorgba";
+
+export const Button = styled.button`
+  width: 100%;
+  height: 48px;
+  border-radius: 8px;
+
+  color: ${({ theme }) => theme.color.white};
+  background-color: ${({ theme }) => theme.color.percentOrange};
+
+  &:hover {
+    background-color: ${({ theme }) =>
+      hexToRgba(theme.color.percentOrange, 0.9)};
+  }
+
+  &:active {
+    background-color: ${({ theme }) => theme.color.darkOrange};
+  }
+
+  &[data-disabled] {
+    color: ${({ theme }) => theme.color.white};
+    background-color: ${({ theme }) => theme.color.greyScale5};
+    cursor: unset;
+  }
+
+  &[data-disabled]:hover {
+    background-color: ${({ theme }) => theme.color.greyScale4};
+  }
+
+  transition: background-color 0.2s ease-in-out;
+`;
