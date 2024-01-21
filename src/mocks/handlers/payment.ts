28c403e4c6178dd6f8893229f95bffc3443d4371
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-01-22 04:02:10 +0900

    feat: 구매완료내역 조회 api mocking 구현

--- src/mocks/handlers/payment.ts
+++ src/mocks/handlers/payment.ts
@@ -1,9 +1,13 @@
 import { BASE_URL, END_POINTS } from "@constants/api";
 import dummyPaymentInfo from "@mocks/data/dummyPaymentInfo.json";
+import dummyPaymentSuccess from "@mocks/data/dummyPaymentSuccess.json";
 import { http, HttpResponse } from "msw";
 
 export const paymentHandler = [
   http.get(`${BASE_URL + END_POINTS.PAYMENT(":productId")}`, () => {
     return HttpResponse.json(dummyPaymentInfo);
+  }),
+  http.get(`${BASE_URL + END_POINTS.PURCHASE_DETAIL(":productId")}`, () => {
+    return HttpResponse.json(dummyPaymentSuccess);
   }),
 ];
