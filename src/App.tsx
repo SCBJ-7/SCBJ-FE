ca70d26bfdb66955aa5093851a27ec5219036462
Bumang-Cyber <jove0729@naver.com>
2024-01-22 12:35:42 +0900

    fix: 타입 수정

--- src/App.tsx
+++ src/App.tsx
@@ -12,13 +12,13 @@ import { useEffect } from "react";
 function App() {
   // firebase
   const firebaseConfig = {
-    apiKey: "AIzaSyAc4XrxZs2G1EVp-NbpCh5rw9rVgnUG284",
-    authDomain: "scbj-af2e3.firebaseapp.com",
-    projectId: "scbj-af2e3",
-    storageBucket: "scbj-af2e3.appspot.com",
-    messagingSenderId: "177564796245",
-    appId: "1:177564796245:web:6b27b878cbc2ccacf39bdc",
-    measurementId: "G-1YD7ZEM9HM",
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDERID,
+    appId: import.meta.env.VITE_APPID,
+    measurementId: import.meta.env.VITE_MEASUREMENT_ID,
   };
 
   const app = initializeApp(firebaseConfig);
