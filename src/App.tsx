bb36b564d1ae2a89ee301ee6ad4f87b6467a12ce
Bumang-Cyber <jove0729@naver.com>
2024-01-22 11:41:44 +0900

    feat: firebase 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import { initializeApp } from "firebase/app";
+// import { getMessaging, getToken } from "firebase/messaging";
+
 import { Outlet, useLocation } from "react-router-dom";
 import Layout from "./components/layout/Layout.tsx";
 import Toast from "./components/toast/Toast.tsx";
@@ -7,6 +10,24 @@ import { AnimatePresence } from "framer-motion";
 import { useEffect } from "react";
 
 function App() {
+  // firebase
+  const firebaseConfig = {
+    apiKey: "AIzaSyAc4XrxZs2G1EVp-NbpCh5rw9rVgnUG284",
+    authDomain: "scbj-af2e3.firebaseapp.com",
+    projectId: "scbj-af2e3",
+    storageBucket: "scbj-af2e3.appspot.com",
+    messagingSenderId: "177564796245",
+    appId: "1:177564796245:web:6b27b878cbc2ccacf39bdc",
+    measurementId: "G-1YD7ZEM9HM",
+  };
+
+  const app = initializeApp(firebaseConfig);
+  app;
+  // const messaging = getMessaging(app);
+
+  // getToken(messaging, { vapidKey: "BKagOny0KF_2pCJQ3m....moL0ewzQ8rZu" });
+
+  // 토스트
   const toastConfig = useToastStore((state) => state.config);
   const setToastConfig = useToastStore((state) => state.setToastConfig);
   const location = useLocation();
