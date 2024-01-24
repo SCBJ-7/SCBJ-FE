fd4d8bc6f04e7037d66f6dee532f2d34792ee9e4
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-01-24 18:15:20 +0900

    fix: axios instance 사용 안함

--- src/apis/fetchRoom.ts
+++ src/apis/fetchRoom.ts
@@ -1,11 +1,11 @@
-import { axiosInstance } from "@apis/axiosInstance";
-import { END_POINTS } from "@constants/api";
+import { BASE_URL, END_POINTS } from "@constants/api";
 import type { ResponseData } from "@type/responseType";
 import type { RoomData } from "@type/room";
+import axios from "axios";
 
 export const getRoom = async (roomId: string): Promise<RoomData> => {
-  const { data } = await axiosInstance.get<ResponseData<RoomData>>(
-    END_POINTS.ROOM(roomId),
+  const { data } = await axios.get<ResponseData<RoomData>>(
+    BASE_URL + END_POINTS.ROOM(roomId),
   );
   return data.data;
 };
