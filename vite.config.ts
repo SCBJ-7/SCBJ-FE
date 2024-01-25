9be1ce06435abf56ebd313e0275c54d463a7bdf1
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-01-26 04:03:31 +0900

    chore: pwa plugin 제거

--- vite.config.ts
+++ vite.config.ts
@@ -4,57 +4,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import svgr from "vite-plugin-svgr";
-import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    svgr(),
-    VitePWA({
-      registerType: "autoUpdate",
-      includeAssets: ["/favicon.svg", "apple-touch-icon.png", "mask-icon.svg"],
-      workbox: {
-        globPatterns: ["**/*.{js,css,html}", "**/*.{svg,png,jpg,gif}"],
-      },
-      devOptions: {
-        enabled: true,
-      },
-      manifest: {
-        name: "퍼센트 호텔",
-        short_name: "Percent Hotel",
-        theme_color: "#FF7C17",
-        background_color: "#FF7C17",
-        start_url: "./index.html",
-        display: "standalone",
-        orientation: "portrait",
-        icons: [
-          {
-            src: "/icon-192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/icon-512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-          {
-            src: "/icon-192-maskable.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "any maskable",
-          },
-          {
-            src: "/icon-512-maskable.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any maskable",
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [react(), svgr()],
   test: {
     globals: true,
     environment: "jsdom",
