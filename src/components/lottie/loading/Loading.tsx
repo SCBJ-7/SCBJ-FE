c149f5011f544f9ec13610c4085b7d8bebab9027
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-01-27 01:10:39 +0900

    design: 로딩 컴포넌트 사이즈 변경

---
+++ src/components/lottie/loading/Loading.tsx
@@ -0,0 +1,13 @@
+import Lottie from "lottie-react";
+import loading from "@assets/lottie/loading.json";
+
+import * as S from "./Loading.style";
+
+const Loading = () => {
+  return (
+    <S.LoadingWrapper>
+      <Lottie animationData={loading} style={{ width: 180, height: 180 }} />
+    </S.LoadingWrapper>
+  );
+};
+export default Loading;
