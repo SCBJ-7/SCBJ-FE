ab01b41cc689cc0feb617b6f2ab3891147c68755
Bumang-Cyber <jove0729@naver.com>
2024-01-26 13:32:27 +0900

    feat: 알림 없을 때 화면 만들기

--- src/pages/alarmPage/AlarmPage.tsx
+++ src/pages/alarmPage/AlarmPage.tsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
 import * as S from "./AlarmPage.style";
 import { useQuery, useSuspenseQuery } from "@tanstack/react-query";
 import { fetchAlarm } from "@apis/fetchAlarm";
 import { format, parseISO } from "date-fns";
 import { fetchUserInfo } from "@apis/fetchUserInfo";
-import { AlarmType } from "@type/alarm";
+import NoResult from "@components/noResult/NoResult";
+import { PATH } from "@constants/path";
+import Loading from "@components/loading/Loading";
 
 const AlarmPage = () => {
   const { data: userData } = useSuspenseQuery({
@@ -12,18 +13,19 @@ const AlarmPage = () => {
     queryFn: fetchUserInfo,
   });
 
-  const { data: alarmData } = useQuery({
+  const { data: alarmData, isLoading } = useQuery({
     queryKey: ["alarm", userData?.id],
     queryFn: fetchAlarm,
     enabled: !!userData?.id,
   });
 
-  console.log(alarmData);
-  const [messages] = useState<AlarmType[] | undefined>(alarmData);
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
 
   return (
     <S.Container>
-      {messages?.map((item) =>
+      {alarmData?.map((item) =>
         item.isRead ? (
           <S.OldMessage key={item.id}>
             <S.OldMessage>
@@ -41,7 +43,14 @@ const AlarmPage = () => {
           </S.NewMessage>
         ),
       )}
-      {!messages && <div>no result</div>}
+      {!alarmData && (
+        <NoResult
+          title="알림 내역이 없습니다."
+          desc="판매가 이루어지면 알림을 확인하실수 있어요."
+          buttonDesc="돌아가기"
+          navigateTo={PATH.ROOT}
+        />
+      )}
     </S.Container>
   );
 };
