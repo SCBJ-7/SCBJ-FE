ea3c3e8fb6a668e5ec522d525bd5ed6ba51df966
Nayoung Park <139189221+im-na0@users.noreply.github.com>
2024-01-27 04:32:40 +0900

    Revert "[#257] 배포 테스트용"

--- src/utils/getNotificationPermission.ts
+++ src/utils/getNotificationPermission.ts
@@ -5,36 +5,25 @@ async function getNotificationPermission() {
   console.log("권한 요청 중...");
 
   const permission = await Notification.requestPermission();
-
   if (permission === "denied") {
-    alert(
-      "알림을 차단하셨습니다.\n브라우저의 사이트 설정에서 변경하실 수 있습니다.",
-    );
-    return false;
+    console.log("알림 권한 허용 안됨");
+    return;
   }
 
-  console.log(permission);
+  console.log("알림 권한이 허용됨");
 
-  if (permission === "granted") {
-    console.log("알림 권한이 허용됨");
+  const fcmToken = await getToken(messaging, {
+    vapidKey: import.meta.env.VITE_FIREBASE_VAPID,
+  });
 
-    const fcmToken = await getToken(messaging, {
-      vapidKey: import.meta.env.VITE_FIREBASE_VAPID,
-    });
+  if (fcmToken) console.log("token: ", fcmToken);
+  else console.log("Can not get Token");
 
-    if (fcmToken) {
-      console.log("token: ", fcmToken);
-    } else {
-      console.log("FCM 토큰을 가져올 수 없습니다.");
-      return false;
-    }
+  onMessage(messaging, (payload) => {
+    console.log("메시지가 도착했습니다.", payload);
+  });
 
-    onMessage(messaging, (payload) => {
-      console.log("메시지가 도착했습니다.", payload);
-    });
-
-    return fcmToken;
-  }
+  return fcmToken;
 }
 
 export default getNotificationPermission;
