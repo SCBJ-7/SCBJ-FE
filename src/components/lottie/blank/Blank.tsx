dd0c96a54e7d66d221d0513fc716f9b09700b4f2
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-01-27 01:12:03 +0900

    feat: blank, success 컴포넌트 구현

---
+++ src/components/lottie/blank/Blank.tsx
@@ -0,0 +1,7 @@
+import Lottie from "lottie-react";
+import blank from "@assets/lottie/blank.json";
+
+const Blank = () => {
+  return <Lottie animationData={blank} />;
+};
+export default Blank;
---
+++ src/components/lottie/success/Success.tsx
@@ -0,0 +1,7 @@
+import Lottie from "lottie-react";
+import success from "@assets/lottie/success.json";
+
+const Success = () => {
+  return <Lottie animationData={success} loop={false} />;
+};
+export default Success;
