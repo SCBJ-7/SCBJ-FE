03368d0f50c6dbb1fc0f1a8c33e95cb3112f2e47
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-01-27 01:15:16 +0900

    feat: 양도글 작성 완료 페이지 로티 애니메이션 적용

--- src/pages/transferWritingSuccessPage/TransferWritingSuccess.tsx
+++ src/pages/transferWritingSuccessPage/TransferWritingSuccess.tsx
@@ -1,11 +1,11 @@
 import * as S from "./TransferWritingSuccess.style";
-import PercentHotelIcon from "@assets/icons/ic_percent_hotel.png";
 import PercentHotelLogo from "@assets/logos/Percent-hotel_logo_b.png";
 import { useNavigate } from "react-router-dom";
 import { PATH } from "@constants/path";
 import { useSearchParams } from "react-router-dom";
 import AccountBottomSheet from "./accountBottomSheet/AccountBottomSheet";
 import { useEffect, useState } from "react";
+import Success from "@/components/lottie/success/Success";
 
 const TransferWritingSuccess = () => {
   const [content, setContent] = useState<
@@ -28,24 +28,26 @@ const TransferWritingSuccess = () => {
   };
 
   return (
-    <S.Container>
+    <S.PageContainer>
       <S.MainWrapper>
         <S.PercentHotelLogo>
           <img src={PercentHotelLogo} alt="퍼센트호텔 로고" />
         </S.PercentHotelLogo>
-        <S.PercentHotelIcon>
-          <img src={PercentHotelIcon} alt="퍼센트호텔 아이콘" />
-        </S.PercentHotelIcon>
+        <S.LottieWrapper>
+          <Success />
+        </S.LottieWrapper>
         <S.TitleWrapper>
           <S.Title>판매 게시물 등록 완료!</S.Title>
           <S.SubTitle>상품이 판매되면 알림으로 알려드릴게요!</S.SubTitle>
         </S.TitleWrapper>
       </S.MainWrapper>
-      <S.Button variant="solid" onClick={doneHandler}>
-        내 판매내역 확인하기
-      </S.Button>
+      <S.BottomWrapper>
+        <S.Button variant="solid" onClick={doneHandler}>
+          내 판매내역 확인하기
+        </S.Button>
+      </S.BottomWrapper>
       <AccountBottomSheet content={content} onSetContent={setContent} />
-    </S.Container>
+    </S.PageContainer>
   );
 };
 
