86056cf7df55b5508889bf7851313c1ff6779e70
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-01-27 04:31:55 +0900

    fix: 타입 수정

--- src/utils/getNotificationPermission.ts
+++ src/utils/getNotificationPermission.ts
@@ -10,15 +10,17 @@ async function getNotificationPermission() {
     alert(
       "알림을 차단하셨습니다.\n브라우저의 사이트 설정에서 변경하실 수 있습니다.",
     );
-    return false;
+    return;
   }
 
   console.log(permission);
 
+  let fcmToken;
+
   if (permission === "granted") {
     console.log("알림 권한이 허용됨");
 
-    const fcmToken = await getToken(messaging, {
+    fcmToken = await getToken(messaging, {
       vapidKey: import.meta.env.VITE_FIREBASE_VAPID,
     });
 
@@ -26,15 +28,15 @@ async function getNotificationPermission() {
       console.log("token: ", fcmToken);
     } else {
       console.log("FCM 토큰을 가져올 수 없습니다.");
-      return false;
+      return;
     }
 
     onMessage(messaging, (payload) => {
       console.log("메시지가 도착했습니다.", payload);
     });
-
-    return fcmToken;
   }
+
+  return fcmToken;
 }
 
 export default getNotificationPermission;
