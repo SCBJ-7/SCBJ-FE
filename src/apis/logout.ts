1b9babeed958c47d440ca3eeaadb8eeea4654c70
Bumang-Cyber <jove0729@naver.com>
2024-01-28 02:38:47 +0900

    feat: 토큰 저장 로직 제거

--- src/apis/logout.ts
+++ src/apis/logout.ts
@@ -1,15 +1,9 @@
 import { axiosInstance } from "@apis/axiosInstance";
-import {
-  ACCESS_TOKEN,
-  END_POINTS,
-  FCM_TOKEN,
-  REFRESH_TOKEN,
-} from "@/constants/api";
+import { ACCESS_TOKEN, END_POINTS, REFRESH_TOKEN } from "@/constants/api";
 
 export const logout = async () => {
   await axiosInstance.post(`${END_POINTS.USER_INFO}/logout`, {
     accessToken: localStorage.getItem(ACCESS_TOKEN),
     refreshToken: localStorage.getItem(REFRESH_TOKEN),
   });
-  localStorage.removeItem(FCM_TOKEN);
 };
