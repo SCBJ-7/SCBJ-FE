1ce848b17ae88177d60cefe5c030668a4f86d162
Bumang-Cyber <jove0729@naver.com>
2024-01-27 20:51:48 +0900

    feat: 사파리 브라우저에서 알림 들어갔을 때 예외처리

--- src/pages/alarmPage/AlarmPage.tsx
+++ src/pages/alarmPage/AlarmPage.tsx
@@ -6,8 +6,11 @@ import { fetchUserInfo } from "@apis/fetchUserInfo";
 import NoResult from "@components/noResult/NoResult";
 import { PATH } from "@constants/path";
 import Loading from "@/components/lottie/loading/Loading";
+import { isMobileSafari } from "@/utils/isMobileSafari";
 
 const AlarmPage = () => {
+  const mobileSafari = isMobileSafari();
+
   const { data: userData } = useSuspenseQuery({
     queryKey: ["UserInfo"],
     queryFn: fetchUserInfo,
@@ -16,41 +19,54 @@ const AlarmPage = () => {
   const { data: alarmData, isLoading } = useQuery({
     queryKey: ["alarm", userData?.id],
     queryFn: fetchAlarm,
-    enabled: !!userData?.id,
+    enabled: !!userData?.id && !mobileSafari,
   });
 
   if (isLoading) {
     return <Loading />;
   }
 
-  return (
-    <S.Container>
-      {alarmData?.map((item) =>
-        item.isRead ? (
-          <S.OldMessage key={item.id}>
-            <h1>{item.content}</h1>
-            <h3>{format(parseISO(item.date), "yyyy. MM. dd. HH:mm")}</h3>
-          </S.OldMessage>
-        ) : (
-          <S.NewMessage key={item.id}>
-            <h1>{item.content}</h1>
-            <div>
-              {!item.isRead && <section />}
+  if (mobileSafari) {
+    return (
+      <NoResult
+        title="알림을 사용할 수 없는 브라우저입니다"
+        desc="모바일 사파리 브라우저에서 알림을 사용할 수 없어요"
+        buttonDesc="홈 화면으로 돌아가기"
+        navigateTo={PATH.ROOT}
+      />
+    );
+  }
+
+  if (alarmData) {
+    return (
+      <S.Container>
+        {alarmData?.map((item) =>
+          item.isRead ? (
+            <S.OldMessage key={item.id}>
+              <h1>{item.content}</h1>
               <h3>{format(parseISO(item.date), "yyyy. MM. dd. HH:mm")}</h3>
-            </div>
-          </S.NewMessage>
-        ),
-      )}
-      {alarmData && alarmData.length === 0 && (
-        <NoResult
-          title="알림 내역이 없습니다."
-          desc="판매가 이루어지면 알림을 확인하실수 있어요."
-          buttonDesc="돌아가기"
-          navigateTo={PATH.ROOT}
-        />
-      )}
-    </S.Container>
-  );
+            </S.OldMessage>
+          ) : (
+            <S.NewMessage key={item.id}>
+              <h1>{item.content}</h1>
+              <div>
+                {!item.isRead && <section />}
+                <h3>{format(parseISO(item.date), "yyyy. MM. dd. HH:mm")}</h3>
+              </div>
+            </S.NewMessage>
+          ),
+        )}
+        {alarmData && alarmData.length === 0 && (
+          <NoResult
+            title="알림 내역이 없습니다."
+            desc="판매가 이루어지면 알림을 확인하실수 있어요."
+            buttonDesc="돌아가기"
+            navigateTo={PATH.ROOT}
+          />
+        )}
+      </S.Container>
+    );
+  }
 };
 
 export default AlarmPage;
