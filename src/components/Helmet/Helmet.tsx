632b580122b2bf36c3f12b60c7faf9633ebe983e
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-01-28 20:45:17 +0900

    fix: react-helmet-async적용

--- src/components/Helmet/Helmet.tsx
+++ src/components/Helmet/Helmet.tsx
@@ -6,16 +6,23 @@ export const HelmetTag = ({ text }: { text: string }) => {
     <Helmet>
       <meta charSet="utf-8" />
       <title>{text} | 퍼센트호텔</title>
-      <meta name="퍼센트호텔" content={text} data-react-helmet="true"></meta>
-      <meta property="og:site_name" content="퍼센트 호텔" />
-      <meta property="og:title" content={text} />
+      <meta name="description" content="양도 거래? 취소 보다 빠른 거래!"></meta>
+
+      <meta itemProp="name" content="퍼센트 호텔" />
+      <meta itemProp="description" content="양도 거래? 취소 보다 빠른 거래!" />
+      <meta itemProp="image" content="/icon-192.png" />
+
       <meta property="og:url" content={PATH.ROOT} />
       <meta property="og:type" content="website" />
+      <meta property="og:title" content={text} />
+      <meta property="og:site_name" content="퍼센트 호텔" />
+      <meta property="og:image" content="/icon-192.png" />
       <meta
         property="og:description"
         content="양도 거래? 취소 보다 빠른 거래!"
       />
-      <meta property="twitter:card" content="summary" />
+
+      <meta property="twitter:card" content="/icon-192.png" />
       <meta name="twitter:title" content={text} />
       <meta
         name="twitter:description"
