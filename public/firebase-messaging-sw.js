7c1ba26ce6cd1720fcf69a7e8d76e536fa75e35a
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-02-06 16:35:47 +0900

    fix: fcm background message만 남겨두기

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -40,20 +40,20 @@ self.addEventListener("activate", function () {
   console.log("fcm sw activate..");
 });
 
-self.addEventListener("push", function (e) {
-  console.log("push: ", e.data.json());
-  if (!e.data.json()) return;
-
-  const resultData = e.data.json().data;
-  const notificationTitle = resultData.title;
-  const notificationOptions = {
-    body: resultData.message,
-    icon: "/icon-192.png",
-  };
-  console.log("push: ", { notificationTitle, notificationOptions });
-
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
+// self.addEventListener("push", function (e) {
+//   console.log("push: ", e.data.json());
+//   if (!e.data.json()) return;
+//
+//   const resultData = e.data.json().data;
+//   const notificationTitle = resultData.title;
+//   const notificationOptions = {
+//     body: resultData.message,
+//     icon: "/icon-192.png",
+//   };
+//   console.log("push: ", { notificationTitle, notificationOptions });
+//
+//   self.registration.showNotification(notificationTitle, notificationOptions);
+// });
 
 self.addEventListener("notificationclick", function (event) {
   console.log("notification click");
@@ -70,6 +70,7 @@ messaging.onBackgroundMessage(function (payload) {
     body: payload.data.message,
     icon: "/icon-192.png",
   };
+  console.log("push: ", { notificationTitle, notificationOptions });
 
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
