8888ad48fb51980fe802d609c562a7560c69fe0c
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-03-14 23:11:49 +0900

    chore: meta tag description 추가

--- src/components/Helmet/Helmet.tsx
+++ src/components/Helmet/Helmet.tsx
@@ -1,33 +1,30 @@
-import { PATH } from "@/constants/path";
 import { Helmet } from "react-helmet-async";
+
+import { PATH } from "@/constants/path";
 
 export const HelmetTag = ({ text }: { text: string }) => {
   return (
     <Helmet>
       <title>{text} | 퍼센트호텔</title>
-      <meta name="description" content="양도 거래? 취소 보다 빠른 거래!"></meta>
 
-      <meta itemProp="name" content="퍼센트 호텔" />
-      <meta itemProp="description" content="양도 거래? 취소 보다 빠른 거래!" />
-      <meta itemProp="image" content="/icon-192.png" />
+      <meta itemProp="name" content="퍼센트 호텔"></meta>
+      <meta
+        itemProp="description"
+        content="양도 거래? 취소 보다 빠른 거래!"
+      ></meta>
+      <meta itemProp="image" content="/icon-192.png"></meta>
 
-      <meta property="og:url" content={PATH.ROOT} />
-      <meta property="og:type" content="website" />
-      <meta property="og:title" content={text} />
-      <meta property="og:site_name" content="퍼센트 호텔" />
-      <meta property="og:image" content="/icon-192.png" />
+      <meta property="og:url" content={PATH.ROOT}></meta>
+      <meta property="og:title" content={`${text} | 퍼센트호텔`}></meta>
+      <meta property="og:site_name" content="퍼센트 호텔"></meta>
+      <meta property="og:type" content="website"></meta>
+      <meta property="og:image" content="/icon-192.png"></meta>
       <meta
         property="og:description"
         content="양도 거래? 취소 보다 빠른 거래!"
-      />
+      ></meta>
 
-      <meta property="twitter:card" content="/icon-192.png" />
-      <meta name="twitter:title" content={text} />
-      <meta
-        name="twitter:description"
-        content="양도 거래? 취소 보다 빠른 거래!"
-      />
-      <meta name="twitter:image" content="/src/assets/logos/main_logo.svg" />
+      <meta name="twitter:card" content="summary"></meta>
     </Helmet>
   );
 };
