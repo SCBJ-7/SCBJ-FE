37fdcf6a00266067f0064806fef6589400243196
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-05-09 19:26:44 +0900

    feat: polymorphic 컴포넌트 d.ts 추가

---
+++ src/components/ui/polymorphic.d.ts
@@ -0,0 +1,21 @@
+import React from "react";
+
+type PolymorphicRef<C extends React.ElementType> =
+  ComponentPropsWithRef<C>["ref"];
+
+type AsProp<C extends React.ElementType> = {
+  as?: C;
+};
+
+type PropsToOmit<C extends React.ElementType, P> = keyof (AsProp<C> & P);
+
+export type PolymorphicComponentProp<
+  C extends React.ElementType,
+  Props = object,
+> = React.PropsWithChildren<Props & AsProp<C>> &
+  Omit<React.ComponentPropsWithoutRef<C>, PropsToOmit<C, Props>>;
+
+export type PolymorphicComponentPropsWithRef<
+  C extends React.ElementType,
+  Props = object,
+> = PolymorphicComponentProp<C, Props> & { ref?: PolymorphicRef<C> };
