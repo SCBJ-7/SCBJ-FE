2677b1874b90c0b8ed49bfa78ec93eeff925c449
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-05-09 18:24:39 +0900

    refactor: styles 디렉터리 분리

---
+++ src/styles/border.ts
@@ -0,0 +1,8 @@
+const border = {
+  none: 0,
+  strokeThin: "1px solid #E9E9E9",
+  strokeWide: "1px solid #F6F6F6",
+  strokeDash: "1px solid #CDCDCD",
+};
+
+export default border;
---
+++ src/styles/sizes.ts
@@ -0,0 +1,20 @@
+import { spacing } from "./spacing";
+
+const overlay = {
+  sheet: "518px",
+};
+
+const container = {
+  sm: "360px",
+  md: "640px",
+  lg: "768px",
+  xl: "1024px",
+};
+
+const sizes = {
+  space: spacing,
+  ...overlay,
+  container,
+};
+
+export default sizes;
