0adebecad4a42206b789daeb8ed9c21286794838
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-05-09 18:24:39 +0900

    refactor: styles 디렉터리 분리

---
+++ src/styles/color.ts
@@ -0,0 +1,25 @@
+const color = {
+  transparent: "transparent",
+  current: "currentColor",
+  black: "hsl(273 10% 4% / 1);",
+  white: "#FFFFFF",
+
+  lightOrange: "#FFEDDA",
+  percentOrange: "#FF7C17",
+  darkOrange: "#E76500",
+  lightBlue: "#D5EBFF",
+  percentBlue: "#0A76DA",
+  darkBlue: "#004789",
+  yanoljaPink: "#FF3478",
+  cautionRed: "#FF4949",
+
+  greyScale1: "hsl(240 3.8% 46.1%)",
+  greyScale2: "#686868",
+  greyScale3: "#999999",
+  greyScale4: "#B8B8B8",
+  greyScale5: "#CDCDCD",
+  greyScale6: "#E9E9E9",
+  greyScale7: "#F6F6F6",
+};
+
+export default color;
