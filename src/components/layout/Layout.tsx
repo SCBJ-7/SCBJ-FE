1946850af224ec796ab41837c766692fd4af1c4b
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-05-23 19:00:21 +0900

    refactor: Layout 컴포넌트 구조 수정 및 bg 프로퍼티 추가

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -5,26 +5,33 @@ import * as S from "./Layout.style";
 import BottomNav from "./navBottom/NavBottom";
 import A2HS from "../A2HS/A2HS";
 
+import { ColorKeys } from "@/styles/theme.ts";
 import { isMobile } from "@/utils/isMobile";
 
 interface ChildrenProps {
   children: React.ReactNode;
-  isHeaderOn: boolean;
-  isBottomNavOn: boolean;
+  isHeaderOn?: boolean;
+  isBottomNavOn?: boolean;
+  bg?: ColorKeys;
 }
 
-const Layout = ({ children, isHeaderOn, isBottomNavOn }: ChildrenProps) => {
+const Layout = ({
+  children,
+  isHeaderOn = false,
+  isBottomNavOn = false,
+  bg = "white",
+}: ChildrenProps) => {
   const isMobileDevice = isMobile();
 
   return (
-    <S.Container>
-      <S.Wrapper>
-        {isHeaderOn && <Header />}
+    <>
+      {isHeaderOn && <Header />}
+      <S.Wrapper bg={bg}>
         {children}
         <A2HS />
-        {isBottomNavOn && <BottomNav isMobile={isMobileDevice} />}
       </S.Wrapper>
-    </S.Container>
+      {isBottomNavOn && <BottomNav isMobile={isMobileDevice} />}
+    </>
   );
 };
 
