d71134926480f33778f51a8b94ca7a939be4bcb9
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-05-23 19:00:35 +0900

    feat: Typo 컴포넌트 추가

---
+++ src/components/ui/typo/typo.tsx
@@ -0,0 +1,44 @@
+import { forwardRef, Children, isValidElement, ElementType } from "react";
+
+import {
+  type IStyledTextProps,
+  type StyledEmEmProps,
+  StyledTypo,
+  StyledEm,
+} from "./typo.styles.ts";
+
+import type {
+  PolymorphicComponentPropsWithRef,
+  PolymorphicRef,
+} from "../polymorphic";
+
+export type TextProps<C extends ElementType = "span"> =
+  PolymorphicComponentPropsWithRef<C, IStyledTextProps & StyledEmEmProps>;
+
+export const Typo = forwardRef(function Typo<C extends ElementType = "span">(
+  props: TextProps<C>,
+  ref?: PolymorphicRef<C>,
+) {
+  const { as, color, typo, isEllipsis, children, ...rest } = props;
+
+  const styledChildren = Children.map(children, (child) => {
+    if (isValidElement(child) && child.type === "em") {
+      const color = child.props.color;
+      return <StyledEm color={color}>{child.props.children}</StyledEm>;
+    }
+    return child;
+  });
+
+  return (
+    <StyledTypo
+      ref={ref}
+      as={as}
+      color={color}
+      typo={typo}
+      isEllipsis={isEllipsis}
+      {...rest}
+    >
+      {styledChildren}
+    </StyledTypo>
+  );
+});
