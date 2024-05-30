347f23e1108381088369d891e745a75fc77cefc6
Park Nayoung <139189221+im-na0@users.noreply.github.com>
2024-05-30 18:41:36 +0900

    feat: Layout 컴포넌트 padding 프로퍼티 추가

--- src/components/layout/Layout.style.ts
+++ src/components/layout/Layout.style.ts
@@ -2,9 +2,32 @@ import { styled } from "styled-components";
 
 import type { ColorKeys } from "@/styles/theme.ts";
 
+import { remCalc } from "@/utils/styleFormatter.ts";
+
+export type LayoutStyleProps = {
+  bg?: ColorKeys;
+  /**
+   * padding-top
+   */
+  pt?: number;
+  /**
+   * padding-bottom
+   */
+  pb?: number;
+  /**
+   * padding-block
+   */
+  paddingBlock?: number;
+  /**
+   * padding-inline
+   */
+  paddingInline?: number;
+};
+
 export const Wrapper = styled.div.withConfig({
-  shouldForwardProp: (prop) => prop !== "bg",
-})<{ bg: ColorKeys }>`
+  shouldForwardProp: (prop) =>
+    !["bg", "pt", "pb", "paddingBlock", "paddingInline"].includes(prop),
+})<LayoutStyleProps>`
   width: 100%;
   min-height: 100%;
   max-width: 768px;
@@ -12,5 +35,13 @@ export const Wrapper = styled.div.withConfig({
   position: relative;
   margin: 0 auto;
 
-  background-color: ${({ theme, bg }) => theme.color[bg]};
+  background-color: ${({ theme, bg }) =>
+    bg ? theme.color[bg] : theme.color.white};
+  padding-top: ${({ pt }) => (pt ? `calc(56px + ${remCalc(pt)})` : undefined)};
+  padding-bottom: ${({ pb }) =>
+    pb ? `calc(78px + ${remCalc(pb)})` : undefined};
+  padding-block: ${({ paddingBlock }) =>
+    paddingBlock ? remCalc(paddingBlock) : undefined};
+  padding-inline: ${({ paddingInline }) =>
+    paddingInline ? remCalc(paddingInline) : undefined};
 `;
