5beb79492f35cb2bbe4222043e100b6d74ac796b
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-06-13 20:00:32 +0900

    feat: 찜 등록, 삭제, 조회 요청 훅 추가

--- src/hooks/api/useWishQuery.ts
+++ src/hooks/api/useWishQuery.ts
@@ -1,18 +1,54 @@
-import { useMutation, useSuspenseQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
-import { deleteWish, getWish } from "@/apis/fetchWish.ts";
+import { deleteWish, getWish, postWish } from "@/apis/fetchWish.ts";
 
 export function useWishQuery() {
-  return useSuspenseQuery({
+  return useQuery({
     queryKey: ["wish"],
     queryFn: getWish,
   });
 }
 
 export function useDeleteWishMutation() {
+  const queryClient = useQueryClient();
+
   const { mutate } = useMutation({
     mutationFn: deleteWish,
+    onMutate: async (newLike) => {
+      await queryClient.cancelQueries({ queryKey: ["wish"] });
+      const previousWish = queryClient.getQueryData(["wish"]);
+      queryClient.setQueryData(["wish"], newLike);
+      return { previousWish };
+    },
+    onError: (err, newLike, context) => {
+      queryClient.setQueryData(["wish"], context?.previousWish);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ["wish"] });
+    },
   });
 
   return { deleteWish: mutate };
+}
+
+export function usePostWishMutation() {
+  const queryClient = useQueryClient();
+
+  const { mutate } = useMutation({
+    mutationFn: postWish,
+    onMutate: async (newLike) => {
+      await queryClient.cancelQueries({ queryKey: ["wish"] });
+      const previousWish = queryClient.getQueryData(["wish"]);
+      queryClient.setQueryData(["wish"], newLike);
+      return { previousWish };
+    },
+    onError: (err, newLike, context) => {
+      queryClient.setQueryData(["wish"], context?.previousWish);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ["wish"] });
+    },
+  });
+
+  return { postWish: mutate };
 }
