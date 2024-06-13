d1c17b86614542800ec7f3a93e15f91f89a79eea
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-06-13 19:18:48 +0900

    fix: 로그인 상태에는 토큰을 사용하도록 수정

--- src/apis/fetchRoom.ts
+++ src/apis/fetchRoom.ts
@@ -1,20 +1,19 @@
-import { ACCESS_TOKEN, BASE_URL, END_POINTS } from "@/constants/api";
 import axios from "axios";
 
 import type { ResponseData } from "@/types/responseType";
 import type { RoomData } from "@/types/room";
 
+import { ACCESS_TOKEN, BASE_URL, END_POINTS } from "@/constants/api";
+
 export const getRoom = async (
   productId: string,
   isLoggedIn: boolean,
 ): Promise<RoomData> => {
-  const headers: { [key: string]: string } = {};
+  const headers: Record<string, string> = {};
 
   if (isLoggedIn) {
     const accessToken = localStorage.getItem(ACCESS_TOKEN);
-    if (!accessToken) {
-      headers["Authorization"] = `Bearer ${accessToken}`;
-    }
+    headers["Authorization"] = `${accessToken}`;
   }
 
   const { data } = await axios.get<ResponseData<RoomData>>(
