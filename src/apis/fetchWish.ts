bcc3d76ea5decb927bd890977463b202f1275cec
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-06-13 19:57:59 +0900

    fix: 잘못된 url로 요청 수정

--- src/apis/fetchWish.ts
+++ src/apis/fetchWish.ts
@@ -2,17 +2,19 @@ import type { ResponseData } from "@/types/responseType.ts";
 import type { WishDataType } from "@/types/wish.ts";
 
 import { axiosInstance } from "@/apis/axiosInstance.ts";
-import { BASE_URL, END_POINTS } from "@/constants/api.ts";
+import { END_POINTS } from "@/constants/api.ts";
 
 export const deleteWish = async (productId: string): Promise<void> => {
-  return await axiosInstance.delete<void>(
-    `${BASE_URL}${END_POINTS.WISH(productId)}`,
-  );
+  return await axiosInstance.delete(`${END_POINTS.WISH(productId)}`);
+};
+
+export const postWish = async (productId: string): Promise<void> => {
+  return await axiosInstance.post(`${END_POINTS.WISH(productId)}`);
 };
 
 export const getWish = async (): Promise<WishDataType> => {
   const { data } = await axiosInstance.get<ResponseData<WishDataType>>(
-    `${BASE_URL}${END_POINTS.WISH_LIST}`,
+    `${END_POINTS.WISH_LIST}`,
   );
   return data.data;
 };
