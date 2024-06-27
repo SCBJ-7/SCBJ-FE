1da5f4f05f128b24a1ea485f832313b95c6431e2
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-06-27 19:59:55 +0900

    refactor: 로그인 상태 여부에 따른 클라이언트 변경

--- src/apis/fetchRoom.ts
+++ src/apis/fetchRoom.ts
@@ -1,25 +1,22 @@
 import axios from "axios";
 
+import { axiosInstance } from "./axiosInstance";
+
 import type { ResponseData } from "@/types/responseType";
 import type { RoomData } from "@/types/room";
 
-import { ACCESS_TOKEN, BASE_URL, END_POINTS } from "@/constants/api";
+import { BASE_URL, END_POINTS } from "@/constants/api";
 
 export const getRoom = async (
   productId: string,
   isLoggedIn: boolean,
 ): Promise<RoomData> => {
-  const headers: Record<string, string> = {};
+  const client = isLoggedIn ? axiosInstance : axios;
+  const url = isLoggedIn
+    ? END_POINTS.ROOM(productId)
+    : `${BASE_URL}${END_POINTS.ROOM(productId)}`;
 
-  if (isLoggedIn) {
-    const accessToken = localStorage.getItem(ACCESS_TOKEN);
-    headers["Authorization"] = `${accessToken}`;
-  }
-
-  const { data } = await axios.get<ResponseData<RoomData>>(
-    `${BASE_URL}${END_POINTS.ROOM(productId)}`,
-    { headers },
-  );
+  const { data } = await client.get<ResponseData<RoomData>>(url);
 
   return data.data;
 };
