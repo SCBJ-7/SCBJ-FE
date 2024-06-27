00fda88fc611383fe5c6b2ff8db1005da2815559
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-06-27 17:07:56 +0900

    feat: 찜 카드 마운트 & 언마운트 시 모션 추가

--- src/pages/wishListPage/components/wishCard/WishCard.tsx
+++ src/pages/wishListPage/components/wishCard/WishCard.tsx
@@ -1,3 +1,6 @@
+import { AnimatePresence, motion } from "framer-motion";
+import { useEffect, useState } from "react";
+
 import WishButton from "./LikeButton.tsx";
 import * as S from "./WishCard.styles.ts";
 
@@ -8,38 +11,85 @@ import { Typo } from "@/components/ui/typo";
 import { formatDateWithoutTime } from "@/utils/dateFormatter.ts";
 
 const WishCard = ({ product }: { product: ProductType }) => {
+  const [isVisible, setIsVisible] = useState(true);
+
   const checkInDate = formatDateWithoutTime(product.checkInDate);
   const checkOutDate = formatDateWithoutTime(product.checkOutDate);
 
+  useEffect(() => {
+    console.log(product.visible);
+    if (product.visible === false) {
+      const timer = setTimeout(() => {
+        setIsVisible(false);
+      }, 1000);
+
+      return () => clearTimeout(timer);
+    } else {
+      setIsVisible(true);
+    }
+  }, [product.visible]);
+
   return (
-    <S.CardContainer>
-      <S.CardWrapper>
-        <S.ImgWrapper>
-          <ProgressiveImg
-            src={product.imageUrl}
-            alt={`${product.hotelName} 이미지`}
-          />
-        </S.ImgWrapper>
-        <S.InfoWrapper>
-          <S.TitleWrapper>
-            <S.TextWrapper>
-              <Typo typo="button4">{product.hotelName}</Typo>
-              <Typo typo="body5">{product.bedType}</Typo>
-            </S.TextWrapper>
-            <WishButton productId={product.productId} />
-          </S.TitleWrapper>
-          <S.TextWrapper>
-            <Typo typo="body5">
-              {checkInDate} ~ {checkOutDate}
-            </Typo>
-            <Typo typo="button4">
-              <span>{product.price.toLocaleString()}원</span>
-            </Typo>
-          </S.TextWrapper>
-        </S.InfoWrapper>
-      </S.CardWrapper>
-    </S.CardContainer>
+    <AnimatePresence initial={false}>
+      {isVisible && (
+        <S.CardContainer
+          as={motion.div}
+          initial="initial"
+          animate="visible"
+          exit="hidden"
+          variants={cardVariants}
+          layout
+        >
+          <S.CardWrapper>
+            <S.ImgWrapper>
+              <ProgressiveImg
+                src={product.imageUrl}
+                alt={`${product.hotelName} 이미지`}
+              />
+            </S.ImgWrapper>
+            <S.InfoWrapper>
+              <S.TitleWrapper>
+                <S.TextWrapper>
+                  <Typo typo="button4">{product.hotelName}</Typo>
+                  <Typo typo="body5">{product.bedType}</Typo>
+                </S.TextWrapper>
+                <WishButton productId={product.id} />
+              </S.TitleWrapper>
+              <S.TextWrapper>
+                <Typo typo="body5">
+                  {checkInDate} ~ {checkOutDate}
+                </Typo>
+                <Typo typo="button4">
+                  <span>{product.price.toLocaleString()}원</span>
+                </Typo>
+              </S.TextWrapper>
+            </S.InfoWrapper>
+          </S.CardWrapper>
+        </S.CardContainer>
+      )}
+    </AnimatePresence>
   );
 };
 
 export default WishCard;
+
+const cardVariants = {
+  initial: {
+    opacity: 0,
+    y: 50,
+    scale: 0.95,
+    transition: { duration: 0.35 },
+  },
+  hidden: {
+    opacity: 0,
+    y: -10,
+    scale: 0.95,
+    transition: { duration: 0.35 },
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    scale: 1,
+    transition: { duration: 0.35 },
+  },
+};
