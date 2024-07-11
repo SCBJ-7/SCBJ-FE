fc3395144b522189703b9544977dc62a44fa3674
chaeminseok <minseok23491@naver.com>
2024-07-11 23:53:29 +0900

    Fix issues in tsconfig and vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// <reference types="vitest" />
+
+import path from "path";
+
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), svgr()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./tests/unitTestsSrc/setup.ts",
+    dir: "./tests/unitTestsSrc",
+  },
+  resolve: {
+    preserveSymlinks: true,
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
