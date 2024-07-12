84b79898bb0f6946bac9459d6a470f4820c23f06
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-07-12 19:07:31 +0900

    feat: 설정 페이지 json-ld 추가

--- src/pages/myPage/setting/Setting.tsx
+++ src/pages/myPage/setting/Setting.tsx
@@ -2,12 +2,25 @@ import * as S from "./Setting.style";
 import Info from "../info/Info";
 import Manage from "../manage/Manage";
 
+import { HelmetTag } from "@/components/Helmet/Helmet";
+
 const Setting = () => {
+  const schemaData = {
+    "@type": "WebPage",
+    mainEntityOfPage: {
+      "@type": "WebPage",
+      "@id": `${location.origin}${location.pathname}}`,
+    },
+  };
+
   return (
-    <S.SettingListContainer>
-      <Manage />
-      <Info />
-    </S.SettingListContainer>
+    <>
+      <HelmetTag title="설정" schemaData={schemaData} />
+      <S.SettingListContainer>
+        <Manage />
+        <Info />
+      </S.SettingListContainer>
+    </>
   );
 };
 
