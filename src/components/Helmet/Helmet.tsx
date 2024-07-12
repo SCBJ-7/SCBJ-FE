f182aaa3d9fa8e1e9366bada4741f05c8902a7ea
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-07-12 19:04:50 +0900

    feat: Helmet Tag 컴포넌트에 스키마 추가
    json-dl 작업을 위해 스키마를 props로 받을 수 있도록 수정

--- src/components/Helmet/Helmet.tsx
+++ src/components/Helmet/Helmet.tsx
@@ -1,39 +1,70 @@
+import { memo } from "react";
 import { Helmet } from "react-helmet-async";
 
-export const HelmetTag = ({
-  title,
-  description,
-}: {
-  title?: string;
-  description?: string;
-}) => {
-  const orig = location.origin;
-  const url = location.href;
-  const defaultDescription =
-    "못 가게된 숙박권 더 이상 돈 날리지 말고, 비싼 호캉스도 믿고 거래하자! 특가보다 저렴한 호캉스 예약.";
-  const fullTitle = `퍼센트호텔${title ? ` | ${title}` : ""}`;
+export const HelmetTag = memo(
+  ({
+    title,
+    schemaType = "TravelAgency",
+    schemaData = {},
+  }: {
+    title?: string;
+    description?: string;
+    schemaType?: string;
+    schemaData?: Record<string, any>;
+  }) => {
+    console.log(schemaData);
 
-  return (
-    <Helmet>
-      <title>{fullTitle}</title>
-      <meta name="description" content={description || defaultDescription} />
+    const orig = location.origin;
+    const url = location.href;
+    const defaultDescription =
+      "못 가게된 숙박권 더 이상 돈 날리지 말고, 비싼 호캉스도 믿고 거래하자! 특가보다 저렴한 호캉스 예약.";
+    const fullTitle = `퍼센트호텔${title ? ` | ${title}` : ""}`;
 
-      <meta property="og:url" content={url} />
-      <meta property="og:type" content="website" />
-      <meta property="og:title" content={fullTitle} />
-      <meta property="og:site_name" content="퍼센트 호텔" />
-      <meta property="og:image" content={`${orig}/og-img.png`} />
-      <meta property="og:description" content={defaultDescription} />
+    const baseSchema = {
+      "@context": "https://schema.org",
+      "@type": schemaType,
+      name: `${fullTitle}`,
+      url: orig,
+      logo: `${orig}/assets/logos/main_logo.svg`,
+      description: defaultDescription,
+      address: {
+        "@type": "PostalAddress",
+        addressCountry: "KR",
+      },
+      potentialAction: {
+        "@type": "SearchAction",
+        target: {
+          "@type": "EntryPoint",
+          urlTemplate: `${orig}/search?q={search_term_string}`,
+        },
+        "query-input": "required name=search_term_string",
+      },
+    };
 
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content={fullTitle} />
-      <meta name="twitter:description" content={defaultDescription} />
-      <meta
-        name="twitter:image"
-        content={`${orig}/assets/logos/main_logo.svg`}
-      />
+    const fullSchema = { ...baseSchema, ...schemaData };
 
-      <link rel="canonical" href={`${orig}${location.pathname}`} />
-    </Helmet>
-  );
-};
+    return (
+      <Helmet>
+        <title>{fullTitle}</title>
+        <meta name="description" content={defaultDescription} />
+
+        <meta property="og:url" content={url} />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={fullTitle} />
+        <meta property="og:site_name" content="퍼센트 호텔" />
+        <meta property="og:image" content={`${orig}/og-img.png`} />
+        <meta property="og:description" content={defaultDescription} />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={fullTitle} />
+        <meta name="twitter:description" content={defaultDescription} />
+        <meta
+          name="twitter:image"
+          content={`${orig}/assets/logos/main_logo.svg`}
+        />
+
+        <script type="application/ld+json">{JSON.stringify(fullSchema)}</script>
+      </Helmet>
+    );
+  },
+);
