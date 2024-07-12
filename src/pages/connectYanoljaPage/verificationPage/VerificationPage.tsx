06245a1d102d54979a9731abf3c43b14a5fa7a15
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-07-12 19:10:11 +0900

    feat: 야놀자 인증 페이지 json-ld 추가

--- src/pages/connectYanoljaPage/verificationPage/VerificationPage.tsx
+++ src/pages/connectYanoljaPage/verificationPage/VerificationPage.tsx
@@ -1,10 +1,11 @@
+import { FormProvider, useForm } from "react-hook-form";
+
+import * as S from "./VerificationPage.style";
+
 import YanoljaLogo from "@/assets/logos/Yanolja_CI.png";
 import SubmitButton from "@/pages/connectYanoljaPage/verificationPage/components/submitButton/SubmitButton";
 import TermsAgreementSection from "@/pages/connectYanoljaPage/verificationPage/components/termsAgreementSection/TermsAgreementSection";
 import VerificationSection from "@/pages/connectYanoljaPage/verificationPage/components/verificationSection/VerificationSection";
-import { FormProvider, useForm } from "react-hook-form";
-
-import * as S from "./VerificationPage.style";
 
 const VerificationPage = () => {
   const methods = useForm({
