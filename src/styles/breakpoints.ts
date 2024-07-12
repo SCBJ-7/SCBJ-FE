c5607d66e6f8991c292b71dfe270b031881a218e
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-07-12 19:02:07 +0900

    chore: 주석 추가

--- src/styles/breakpoints.ts
+++ src/styles/breakpoints.ts
@@ -1,10 +1,10 @@
 const breakpoints = {
   base: "0em",
   xs: "24em", // 384px
-  sm: "36em", // 576px
-  md: "48em", // 768 px
-  lg: "62em", // 992 px
-  xl: "80em", // 1280px
+  sm: "36em", // 576px Mobile
+  md: "48em", // 768 px Tablet
+  lg: "62em", // 992 px Desktop
+  xl: "80em", // 1280px Desktop
 };
 
 export default breakpoints;
