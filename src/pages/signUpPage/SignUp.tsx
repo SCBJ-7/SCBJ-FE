b5ff14d88afed46204b7aab3175f561518c9e56c
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-07-12 19:06:31 +0900

    feat: 회원가입,로그인 페이지 json-ld 추가

--- src/pages/signUpPage/SignUp.tsx
+++ src/pages/signUpPage/SignUp.tsx
@@ -1,11 +1,14 @@
-import TermsAgreementSection from "@/pages/connectYanoljaPage/verificationPage/components/termsAgreementSection/TermsAgreementSection";
 import { FormProvider, useForm } from "react-hook-form";
 
 import FieldValues from "./components/fieldValues/FieldValues";
 import SignUpSubmitBtn from "./components/signUpSubmitBtn/SignUpSubmitBtn";
 import * as S from "./SignUp.style";
 
-const SignUp = () => {
+import { HelmetTag } from "@/components/Helmet/Helmet";
+import { PATH } from "@/constants/path";
+import TermsAgreementSection from "@/pages/connectYanoljaPage/verificationPage/components/termsAgreementSection/TermsAgreementSection";
+
+const SignUpView = () => {
   const methods = useForm({
     mode: "onChange",
     shouldUnregister: false,
@@ -24,6 +27,29 @@ const SignUp = () => {
         <SignUpSubmitBtn />
       </FormProvider>
     </S.SignUpContainer>
+  );
+};
+
+const SignUp = () => {
+  const schemaData = {
+    "@type": "WebPage",
+    description:
+      "퍼센트호텔 회원가입 페이지입니다. 가입하시면 다양한 혜택을 누리실 수 있습니다.",
+    mainEntityOfPage: {
+      "@type": "WebPage",
+      "@id": `${location.origin}${PATH.SIGNUP}`,
+    },
+  };
+
+  return (
+    <>
+      <HelmetTag
+        title="회원가입"
+        schemaType="WebPage"
+        schemaData={schemaData}
+      />
+      <SignUpView />
+    </>
   );
 };
 
