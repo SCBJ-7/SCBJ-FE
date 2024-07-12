c5a6f1ee530ae8a1912b6bee4f088d22f435b802
im-na0 <139189221+im-na0@users.noreply.github.com>
2024-07-12 20:10:09 +0900

    fix: 정적 메타 태그로 변경

--- src/components/Helmet/Helmet.tsx
+++ src/components/Helmet/Helmet.tsx
@@ -15,7 +15,6 @@ export const HelmetTag = memo(
     console.log(schemaData);
 
     const orig = location.origin;
-    const url = location.href;
     const defaultDescription =
       "못 가게된 숙박권 더 이상 돈 날리지 말고, 비싼 호캉스도 믿고 거래하자! 특가보다 저렴한 호캉스 예약.";
     const fullTitle = `퍼센트호텔${title ? ` | ${title}` : ""}`;
@@ -47,22 +46,6 @@ export const HelmetTag = memo(
       <Helmet>
         <title>{fullTitle}</title>
         <meta name="description" content={defaultDescription} />
-
-        <meta property="og:url" content={url} />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content={fullTitle} />
-        <meta property="og:site_name" content="퍼센트 호텔" />
-        <meta property="og:image" content={`${orig}/og-img.png`} />
-        <meta property="og:description" content={defaultDescription} />
-
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content={fullTitle} />
-        <meta name="twitter:description" content={defaultDescription} />
-        <meta
-          name="twitter:image"
-          content={`${orig}/assets/logos/main_logo.svg`}
-        />
-
         <script type="application/ld+json">{JSON.stringify(fullSchema)}</script>
       </Helmet>
     );
